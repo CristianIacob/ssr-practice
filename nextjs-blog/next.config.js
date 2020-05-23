7c0d0df0c2222366ff2f948a8e2f9513be8de2e0
Cristian Iacob <ici.faq@gmail.com>
2020-05-23 10:40:12 +0300

    try fixing build id issue

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -1,4 +1,7 @@
+const nextBuildId = require("next-build-id");
+
 module.exports = {
+  generateBuildId: () => nextBuildId({ dir: __dirname }),
   env: {
     PHOTOS_API_KEY: process.env.PHOTOS_API_KEY,
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
