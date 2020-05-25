6e654f030d7b234170ad267ef912d83fe80eca48
Cristian Iacob <ici.faq@gmail.com>
2020-05-25 17:43:13 +0300

    add missing import

--- components/Cards/Card/Weather/index.js
+++ components/Cards/Card/Weather/index.js
@@ -7,7 +7,6 @@ import FailedAPI from "../../../FailedAPI";
 
 function Weather(props) {
   const { data: weatherData, error } = useSWR(`/api/weather${props.name}`, () => weatherInfo(props.name));
-
   /* error handling */
   if (error) return <FailedAPI name={"weather"} />;
 
@@ -18,7 +17,7 @@ function Weather(props) {
   const { sunrise, sunset, country } = sys;
 
   return (
-    <div className="flex flex-col px-4 py-4">
+    <div className='flex flex-col px-4 py-4'>
       <WeatherInfo name={weatherData.name} main={main} icon={weather[0].icon} />
       <WeatherDescription weather={weather[0]} />
     </div>
--- components/Cards/Card/photo.js
+++ components/Cards/Card/photo.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { photos } from "../../../lib/FetchAPI";
 import useSWR from "swr";
+import FailedAPI from "../../FailedAPI";
 
 function Photo(props) {
   const { data: photo, error } = useSWR(`/api/unsplash${props.name}`, () => photos(props.name));
-
   /* error handling */
   if (error) return <FailedAPI name={"image"} />;
 
   if (!photo) return <div>Loading photo...</div>;
-  return <img className="h-56 w-full object-cover" src={photo} alt="" />;
+  return <img className='h-56 w-full object-cover' src={photo} alt='' />;
 }
 
 export default Photo;
