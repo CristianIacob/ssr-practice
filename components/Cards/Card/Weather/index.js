f4c9117cc2c8d4faee6b3444da0957474375f2f4
Cristian Iacob <ici.faq@gmail.com>
2021-07-11 12:18:05 +0300

    Add Loader component.

--- components/Cards/Card/Weather/index.js
+++ components/Cards/Card/Weather/index.js
@@ -4,20 +4,33 @@ import useSWR from "swr";
 import WeatherDescription from "./weatherDescription";
 import WeatherInfo from "./weatherInfo";
 import FailedAPI from "../../../FailedAPI";
+import Loader from "../../../Loader";
 
 function Weather(props) {
+  /**
+   * Fetch data
+   */
   const { data: weatherData, error } = useSWR(`/api/weather${props.name}`, () => weatherInfo(props.name));
-  /* error handling */
+
+  /**
+   * Error handling
+   */
   if (error) return <FailedAPI name={"weather"} />;
 
-  /* loading */
-  if (!weatherData) return <div>Loading weather...</div>;
+  /**
+   * Loader
+   */
+  if (!weatherData)
+    return (
+      <div style={{ height: 143 }} className="flex flex-col px-4 py-4 items-center justify-center">
+        <Loader />
+      </div>
+    );
 
-  const { main, clouds, wind, weather, sys } = weatherData;
-  const { sunrise, sunset, country } = sys;
+  const { main, weather } = weatherData;
 
   return (
-    <div className='flex flex-col px-4 py-4'>
+    <div className="flex flex-col px-4 py-4">
       <WeatherInfo name={weatherData.name} main={main} icon={weather[0].icon} />
       <WeatherDescription weather={weather[0]} />
     </div>
--- components/Cards/Card/photo.js
+++ components/Cards/Card/photo.js
@@ -2,15 +2,27 @@ import React from "react";
 import { photos } from "../../../lib/FetchAPI";
 import useSWR from "swr";
 import FailedAPI from "../../FailedAPI";
+import Loader from "../../Loader";
 
 function Photo(props) {
   const { data: photo, error } = useSWR(`/api/unsplash${props.name}`, () => photos(props.name));
 
-  /* error handling */
+  /**
+   * Error handling
+   */
   if (error) return <FailedAPI name={"image"} />;
 
-  if (!photo) return <div>Loading photo...</div>;
-  return <img className='h-56 w-full object-cover' src={photo} alt='' />;
+  /**
+   * Loader
+   */
+  if (!photo)
+    return (
+      <div className="h-56 w-full flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+
+  return <img className="h-56 w-full object-cover" src={photo} alt="" />;
 }
 
 export default Photo;
